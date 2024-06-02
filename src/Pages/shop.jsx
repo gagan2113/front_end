29300845e98fe899278b1eca49f59e26604fb378
gagan2113 <gaganverma5131@gmail.com>
2024-06-03 03:10:05 +0530

    first commit

---
+++ src/Pages/shop.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Hero from '../Componenets/Hero/hero' 
+const shop = () => {
+    return (
+        <div>
+            <Hero/>
+        </div>
+    )
+}
+
+export default shop
